aec8ef5f2ec6d36fb0843ebf9944728c25de33a3
Pranav Goyal <pranavgoyal.work@gmail.com>
2025-04-09 07:47:39 +0000

    fixing

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  pluginVue.configs["flat/essential"],
+]);
